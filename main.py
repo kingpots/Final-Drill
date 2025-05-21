31d60326e703c943a442417db4da96333efb1802
kingpots <141839529+kingpots@users.noreply.github.com>
2025-05-22 07:38:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root123'
+app.config['MYSQL_PASSWORD'] = '09102001'
 app.config['MYSQL_DB'] = 'mydb'
 
 mysql = MySQL(app)
